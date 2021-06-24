57f1c1b36e4add4e7534a1c82a64f22c7b9c6e21
Hayden <59178038+asianhaydenxd@users.noreply.github.com>
2021-06-24 14:32:29 -0400

    Reorganize player movement script
    More comments have also been added.

--- Pong/Assets/Scripts/PlayerMovement.cs
+++ Pong/Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public static float altitude = 0.0f;
-    
     public static float movementSpeed = 12.0f;
 
     // Update is called once per frame
     void Update()
     {
+        // Move the paddle and keep on screen
         altitude += movementSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
-
         altitude = Mathf.Clamp(altitude, -4.0f, 4.0f);
 
+        // Update the paddle's position
         transform.position = new Vector3(-7.0f, altitude, 0.0f);
     }
 }
